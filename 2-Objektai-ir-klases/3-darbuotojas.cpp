64b2dea6dc2c739eab7d3f0d7bfedfe94fcdf80b
Nerijus Pocevičius <nerpoc42@gmail.com>
2022-02-04 00:17:42 +0200

    Pridėtas 2.3 uždavinio sprendimas

---
+++ 2-Objektai-ir-klases/3-darbuotojas.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+/*
+
+3 Užduotis:
+ - Klasė darbuotojas, su privačiais duomenimis:
+   - amžius
+   - darbo patirtis
+   - atlyginimas
+   - mokesčiai
+ - Skaičiuoja mokesčius
+ - Juos atspausdina
+ - Įrašo į darbuotojo objektą duomenis
+ - Atspausdina darbuotojo objekto duomenis
+
+*/
+
+class Darbuotojas {
+private:
+	unsigned amzius;
+	unsigned darboPatirtis;
+	unsigned atlyginimas;
+	double mokesciai;
+
+	constexpr static double SODRA = 0.09;
+	constexpr static double MOKESCIAI = 0.15;
+
+	//Darbuotojas(unsigned amzius, unsigned darboPatirtis, unsigned atlyginimas) {
+	//	this->amzius = amzius;
+	//	this->darboPatirtis = darboPatirtis;
+	//	this->atlyginimas = atlyginimas;
+	//}
+	
+public:
+	void spausdintiDuomenis() {
+		std::cout << "Amžius: " << amzius << std::endl;
+		std::cout << "Darbo patirtis: " << darboPatirtis << std::endl;
+		std::cout << "Atlyginimas: " << atlyginimas << std::endl;
+		spausdintiMokescius();
+	}
+
+	unsigned gautiAmziu() { return this->amzius; }
+	void keistiAmziu(unsigned amzius) { this->amzius = amzius; }
+
+	unsigned gautiDarboPatirti() { return this->darboPatirtis; }
+	void keistiDarboPatirti(unsigned patirtis) { this->darboPatirtis = patirtis; }
+
+	unsigned gautiAtlyginima() { return this->atlyginimas; }
+	void keistiAtlyginima(unsigned atlyginimas) { this->atlyginimas = atlyginimas; }
+
+	double skaiciuotiMokescius() { return this->mokesciai = atlyginimas * (SODRA+MOKESCIAI); }
+	void spausdintiMokescius() { 
+		std::cout << "Mokesčiai: " << this->mokesciai << std::endl; 
+	}
+};
+
+int main() {
+	Darbuotojas darb;
+
+	unsigned amzius;
+	unsigned darboPatirtis;
+	unsigned atlyginimas;
+
+	std::cout << "Amžius: ";
+	std::cin >> amzius;
+	std::cout << "Darbo patirtis: ";
+	std::cin >> darboPatirtis;
+	std::cout << "Atlyginimas: ";
+	std::cin >> atlyginimas;
+
+	darb.keistiAmziu(amzius);
+	darb.keistiDarboPatirti(darboPatirtis);
+	darb.keistiAtlyginima(atlyginimas);
+
+	darb.skaiciuotiMokescius();
+
+	darb.spausdintiDuomenis();
+
+	return 0;
+}
