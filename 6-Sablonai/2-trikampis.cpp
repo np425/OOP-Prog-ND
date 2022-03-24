cc6db2cacbb87f32545a00a897a8d063fbef10ba
Nerijus Pocevičius <nerpoc42@gmail.com>
2022-03-24 10:02:22 +0200

    Pridėtas 6.2 sprendimas iš naujų užduočių

---
+++ 6-Sablonai/2-trikampis.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cmath> 
+#include <string>
+
+/*
+
+1 Užduotis:
+ - Klasė stačiakampis <T1,T2>
+   - ilgis (T1)
+   - plotis (T2)
+   - plotas (T2)
+ - Sukuria trys stačiakampio objektus:
+   1. <int,int>
+   2. <double,double>
+   3. <int,double>
+ - Juos nuskaito
+ - Atspausdina kiekvieno stačiakampio plotą
+*/
+
+class NetaisyklingasTrikampis {
+	std::string klaida;
+
+public:
+	NetaisyklingasTrikampis(std::string klaida) : klaida(klaida) {}
+	std::string gautiKlaida() const { return klaida; }
+};
+
+class Trikampis {
+	double a, b, c, ilg;
+
+	double ilgKrastine() {
+		if (a > b && a > c) {
+			return a;
+		} 
+		if (b > a && b > c) {
+			return b;
+		}
+		return c;
+	}
+
+public:
+	Trikampis(double a, double b, double c) : a(a), b(b), c(c) {
+		ilg = ilgKrastine();
+		if (a > b && a > c) {
+			ilg = a;
+		}
+		
+		if (a+b < c || a+c < b || b+c < a) {
+			throw NetaisyklingasTrikampis("Ilgiausia kraštinė: " + std::to_string(ilg));
+		}
+	}
+};
+
+int main() {
+	double a, b, c;
+
+	std::cout << "Iveskite kraštinę a: " << std::endl;
+	std::cin >> a;
+
+	std::cout << "Iveskite kraštinę b: " << std::endl;
+	std::cin >> b;
+
+	std::cout << "Iveskite kraštinę c: " << std::endl;
+	std::cin >> c;
+
+	try {
+		Trikampis t(a,b,c);
+		std::cout << "Taisyklingas trikampis" << std::endl;
+	} catch (const NetaisyklingasTrikampis& t) {
+		std::cerr << "Netaisyklingas trikampis" << std::endl;
+		std::cerr << t.gautiKlaida() << std::endl;
+	}
+
+	return 0;
+}
